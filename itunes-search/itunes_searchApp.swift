7fff48984f2b69c01856b4ae7b8d620e85825b40
Scott Moon <47267960+interactord@users.noreply.github.com>
2023-06-13 20:42:34 +0900

    Initial Commit

---
+++ itunes-search/itunes_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  itunes_searchApp.swift
+//  itunes-search
+//
+//  Created by Scott Moon on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct itunes_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
