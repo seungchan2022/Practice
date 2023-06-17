5c7a1958229fe4ae6ec1190d0b9871ee6cd6e468
승찬 <anstmdcks12@naver.com>
2023-06-17 15:31:06 +0900

    - 네비게이션 view안에서 드래그 하면 navigationBar위로 cell들이 올라갈때 searchBar의 배경이 흐릿하게 보여야 되는데 안보인다     => 일단 Header의 색을 바꿔서 네비게이션 안의 view인 것처럼 표현
     - 텍스트 필드 크기 조정, 취소 버튼 크기 조정
    
     - 다른곳을 탭하였을때가 아닌 드래그 하였을때 키보드가 내려가도록
        => 드래그로 바꾼이유: 예를들어 검색을 했는데 화면을 꽉 채우게 검색 결과가 나오면 빈곳을 탭할곳이 없어서 원하지 않은 아이템을 선택할 수 있으므로
        => 문제가 맨위를 드래그 해야 사라진다... -> 아래로 드래그 할때도 없어지게 하려면…?
    
     - 패딩을 주는데 자꾸 짤리길래 무언가 했는데 화면의 너비를 잘못 설정함
        => 화면의 너비를 정의하고 사용할때는 아이템간의 spacing과 padding 모두 신경쓴다음 정의 해주어야 된다!
        ex) 사이즈 정의:   |-|    ||     ||.    |-|
          =>  |padding|  image  |spacing|  image  |spacing|  image  |padding|
          => padding: 10, spacing: 10
          private let imageDimension: CGFloat = (UIScreen.main.bounds.width - 40) / 3

--- itunes-search/Feature/Page/AppSearchPage/AppSearchPage2.swift
+++ itunes-search/Feature/Page/AppSearchPage/AppSearchPage2.swift
@@ -23,15 +23,20 @@ extension AppSearchPage2: View {
   var header: some View {
     VStack {
       HStack {
+        
+        Spacer()
+        
         HStack {
           Image(systemName: "magnifyingglass")
             .renderingMode(.template)
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .frame(width: 20, height: 20)
+            .frame(width: 15, height: 15)
+            .foregroundColor(.gray)
           
           TextField("검색하실 앱의 이름을 적으시오", text: $keyword)
           //                    .foregroundColor(.blue)
+            .font(.system(size: 14, weight: .medium))
             .textFieldStyle(.plain)
           // textField를 선택하면 키보드가 나오고
             .focused($isFocused, equals: true)
@@ -40,11 +45,13 @@ extension AppSearchPage2: View {
             Button(action: { keyword = "" }) {
               Image(systemName: "xmark.circle.fill")
                 .imageScale(.large)
+                .frame(width: 15, height: 15)
                 .foregroundColor(.gray)
             }
           }
         }
-        .frame(maxHeight: 50)
+        //        .frame(maxHeight: 50)
+        .frame(width: 280 ,height: 35)
         .padding(.horizontal, 16)
         .background(
           RoundedRectangle(cornerRadius: 8)
@@ -54,10 +61,14 @@ extension AppSearchPage2: View {
                 .fill(.gray.opacity(0.2)))
         )
         
+        Spacer()
+        
         Button(action: { keyword = "" }) {
           Text("취소")
-            .font(.system(size: 20, weight: .bold))
+            .font(.system(size: 18, weight: .medium))
         }
+        
+        Spacer()
       } // HStack
     } // VStack
     .padding(.top, 10)
@@ -82,18 +93,23 @@ extension AppSearchPage2: View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 15, height: 15)
+                .foregroundColor(.gray)
               
               Text(title)
+                .font(.system(size: 14, weight: .medium))
             }
             Divider().background(Color.white)
           }
         }
         .listRowInsets(EdgeInsets())
+        .listRowSeparator(.hidden)
         .padding(.horizontal, 16)
         .listRowBackground(Color.clear)
       }
     }
     // 다른곳을 탭하였을때가 아닌 드래그 하였을때 키보드가 내려가도록
+    // => 드래그로 바꾼이유: 예를들어 검색을 했는데 화면을 꽉 채우게 검색 결과가 나오면 빈곳을 탭할곳이 없어서 원하지 않은 아이템을 선택할 수 있으므로
+    // => 문제가 맨위를 드래그 해야 사라진다... -> 아래로 드래그 할때도 없어지게 하려면...?
     .scrollDismissesKeyboard(.interactively)
     .listStyle(.plain)
   }
@@ -105,7 +121,11 @@ extension AppSearchPage2: View {
         header
         content
       }
-      //      .background(.black)
+      .background(.black)
+      // 탭 제스처를 사용하면 navigation를 통한 view 이동이 안됌
+      //      .onTapGesture {
+      //        isFocused = false
+      //      }
       // 키워드가 비어있으면 모든 리스트, 키워드를 입력하면 필터링된 텍스트가 나오도록
       .onChange(of: keyword) {
         items = keyword.isEmpty ? mock2 : filter($0)
@@ -119,7 +139,15 @@ let mock2: [String] = [
   "방탈출게임",
   "방치형",
   "방 구하기",
-  "방치형 게임",
+  "전국방탈출",
+  "EndlessRooms",
+  "HeartlessRoom",
+  "Escape Rooms",
+  "방탈출 : 탈출 게임",
+  "탈출 게임",
+  "방탈출 퍼즐 게임",
+  "방탈출 스토리 게임",
+  "방탈출 미스터리 게임",
 ]
 
 struct DetailView {
@@ -141,9 +169,11 @@ extension DetailView: View {
             .renderingMode(.template)
             .resizable()
             .aspectRatio(contentMode: .fit)
-            .frame(width: 20, height: 20)
+            .frame(width: 15, height: 15)
+            .foregroundColor(.gray)
           
           TextField("", text: $keyword)
+            .font(.system(size: 14, weight: .medium))
             .textFieldStyle(.plain)
           // textField를 선택하면 키보드가 나오고
             .focused($isFocused, equals: true)
@@ -151,12 +181,12 @@ extension DetailView: View {
           if !keyword.isEmpty {
             Button(action: { dismiss() }) {
               Image(systemName: "xmark.circle.fill")
-                .imageScale(.large)
+                .frame(width: 15, height: 15)
                 .foregroundColor(.gray)
             }
           }
         }
-        .frame(maxHeight: 50)
+        .frame(width: 280 ,height: 35)
         .padding(.horizontal, 16)
         .background(
           RoundedRectangle(cornerRadius: 8)
@@ -166,13 +196,19 @@ extension DetailView: View {
                 .fill(.gray.opacity(0.2)))
         )
         
+        Spacer()
+        
         Button(action: {  }) {
           Text("취소")
+            .font(.system(size: 18, weight: .medium))
             .onTapGesture {
               keyword = ""
               dismiss()
             }
         }
+        
+        Spacer()
+        
       } // HStack
       
       HStack {
@@ -180,9 +216,9 @@ extension DetailView: View {
           print("DEBUG: 공포 버튼")
         } label: {
           Text("공포")
-            .font(.system(size: 18, weight: .bold))
+            .font(.system(size: 16, weight: .medium))
             .foregroundColor(.blue)
-            .frame(width: 60, height: 40)
+            .frame(width: 60, height: 30)
             .background(
               RoundedRectangle(cornerRadius: 20)
                 .stroke(.gray.opacity(0.5), lineWidth: 1)
@@ -196,7 +232,9 @@ extension DetailView: View {
       }
     } // VStack
     .padding(.top, 10)
-    .padding(.horizontal, 5)
+    .padding(.bottom, 10)
+    .padding(.horizontal, 10)
+    .background(.gray.opacity(0.2))
   }
   
   @ViewBuilder
@@ -210,19 +248,25 @@ extension DetailView: View {
     }
     .background(.black)
     .listStyle(.plain)
+    .scrollDismissesKeyboard(.interactively)
   }
   
   var body: some View {
-    VStack(spacing: 20) {
+    VStack(spacing: .zero) {
       header
       content
+    }
+    .onTapGesture {
+      isFocused = false
     }
   }
 }
 
 struct DetailCellView {
   let post: Post
-  
+  // 사이즈 정의:   |-|    ||     ||.    |-|
+  // =>  |padding|  image  |spacing|  image  |spacing|  image  |padding|
+  // => padding: 10, spacing: 10
   private let imageDimension: CGFloat = (UIScreen.main.bounds.width - 40) / 3
   private var imageHeight: CGFloat {
     imageDimension * 2
@@ -238,14 +282,14 @@ extension DetailCellView: View {
         Image(post.thumbnail)
           .resizable()
           .scaledToFill()
-          .frame(width: 80, height: 80)
+          .frame(width: 60, height: 60)
           .cornerRadius(20)
         VStack(alignment: .leading, spacing: 5) {
           Text(post.title)
-            .font(.system(size: 16, weight: .bold))
+            .font(.system(size: 14, weight: .bold))
             .foregroundColor(.white)
           Text(post.explantion)
-            .font(.system(size: 14, weight: .medium))
+            .font(.system(size: 12, weight: .medium))
             .foregroundColor(.gray)
           HStack(spacing: 4) {
             ForEach(0 ..< 5, id: \.self) { star in
@@ -256,7 +300,7 @@ extension DetailCellView: View {
                 .frame(width: 10, height: 10)
             }
             Text(post.evaluation)
-              .font(.system(size: 12, weight: .light))
+              .font(.system(size: 12, weight: .ultraLight))
           }
           .foregroundColor(.gray)
         }
@@ -269,29 +313,30 @@ extension DetailCellView: View {
             print("DEBUG: Did tap button..")
           } label: {
             Text("받기")
-              .font(.system(size: 18, weight: .bold))
+              .font(.system(size: 14, weight: .bold))
               .foregroundColor(.blue)
-              .frame(width: 100, height: 40)
+              .frame(width: 80, height: 30)
               .background(
                 RoundedRectangle(cornerRadius: 20)
                   .fill(.gray.opacity(0.2))
               )
           }
           Text(post.pathOfPurchase)
-            .font(.system(size: 12, weight: .light))
+            .font(.system(size: 10, weight: .light))
             .foregroundColor(.gray)
         }
+        .padding(.top, 20)
       }
       
       Group {
-        HStack(spacing: 20) {
+        HStack(spacing: 10) {
           // => 인덱스가 짝수이면 하나의 이미지 설명, 홀수이면 3개의 이미지 설명이 나오도록 설정
           if let index = Post.MOCK_POSTS.firstIndex(of: post) {
             if index % 2 == 0 {
               Image(post.imageDescriptions)
                 .resizable()
                 .scaledToFill()
-                .frame(width: UIScreen.main.bounds.width, height: (UIScreen.main.bounds.width / 2) + 30)
+                .frame(width: (UIScreen.main.bounds.width - 20), height: (UIScreen.main.bounds.width / 2) + 30)
                 .clipShape(RoundedRectangle(cornerRadius: 10))
             } else {
               ForEach(0 ..< 3, id: \.self) { index in
@@ -308,6 +353,7 @@ extension DetailCellView: View {
         }
       }
       .padding(.top)
+      
     }
     .padding(.horizontal, 10)
     .padding(.vertical, 20)
@@ -463,5 +509,19 @@ extension Post {
   ]
 }
 
-// 다음에 해결할것
-// - 네비게이션 view안에서 드래그 하면 navigationBar위로 cell들이 올라갈때 배경이 흐릿하게 보여야 되는데 안보인다
+
+
+// - 네비게이션 view안에서 드래그 하면 navigationBar위로 cell들이 올라갈때 searchBar의 배경이 흐릿하게 보여야 되는데 안보인다
+//    => 일단 Header의 색을 바꿔서 네비게이션 안의 view인 것처럼 표현
+// - 텍스트 필드 크기 조정, 취소 버튼 크기 조정
+// - 다른곳을 탭하였을때가 아닌 드래그 하였을때 키보드가 내려가도록
+//    => 드래그로 바꾼이유: 예를들어 검색을 했는데 화면을 꽉 채우게 검색 결과가 나오면 빈곳을 탭할곳이 없어서 원하지 않은 아이템을 선택할 수 있으므로
+//    => 문제가 맨위를 드래그 해야 사라진다... -> 아래로 드래그 할때도 없어지게 하려면...?
+
+// - 패딩을 주는데 자꾸 짤리길래 무언가 했는데 화면의 너비를 잘못 설정함
+//    => 화면의 너비를 정의하고 사용할때는 아이템간의 spacing과 padding 모두 신경쓴다음 정의 해주어야 된다!
+//    ex) 사이즈 정의:   |-|    ||     ||.    |-|
+//      =>  |padding|  image  |spacing|  image  |spacing|  image  |padding|
+//      => padding: 10, spacing: 10
+//      private let imageDimension: CGFloat = (UIScreen.main.bounds.width - 40) / 3
+
